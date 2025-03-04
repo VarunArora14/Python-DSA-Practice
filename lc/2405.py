c456cc21383b682d6a42f90627a4850c5dbf4b0f
Varun Arora <varunarora1408@gmail.com>
2025-03-05 00:24:45 +0530

    2405 refactored

--- lc/2405.py
+++ lc/2405.py
@@ -1,21 +1,16 @@
+class Solution:
+    def partitionString(self, s: str) -> int:
+        curr_substr_set = set()
+        substr_count = 1
+        for i in range(len(s)):
+            if s[i] in curr_substr_set:
+                substr_count += 1
+                curr_substr_set = set([s[i]])
+            else:
+                curr_substr_set.add(s[i])
 
-s = "ssssss"
-start=0
-pos=start
-end=  len(s)
+        return substr_count
 
-store = set()
 
-substrCount=1
-while pos<end:
-    print(pos)
-    if s[pos] in store:
-        substrCount+=1
-        print(f"substr -> start: {start}, end: {pos-1}")
-        start=pos
-        store.clear()
-    else:    
-        store.add(s[pos])
-        pos+=1
-
-print(substrCount)
+s = Solution()
+print(s.partitionString("ssssss"))
