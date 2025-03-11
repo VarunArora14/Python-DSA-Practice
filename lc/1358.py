b46afe16d08334c433dc0fd5e9cc0cf053d8fe7b
Varun Arora <varunarora1408@gmail.com>
2025-03-12 00:24:38 +0530

    daily lc updated

--- lc/1358.py
+++ lc/1358.py
@@ -1,33 +1,31 @@
+from typing import List
+
+
 class Solution:
+    def _check_all_chars_present(self, freq_arr: List[int]):
+        return all([f > 0 for f in freq_arr])
+        # returns True only when all elements of this array is True for condition inside
+
     def numberOfSubstrings(self, s: str) -> int:
-        left = right = total = 0
-        # Track frequency of a, b, c
-        freq = [0] * 3
+        start, end = 0, 0
+        freqs_arr = [0, 0, 0]
+        res = 0
+        while end < len(s):
+            freqs_arr[ord(s[end]) - ord("a")] += 1
 
-        while right < len(s):
-            # Add character at right pointer to frequency array
-            freq[ord(s[right]) - ord("a")] += 1
+            while self._check_all_chars_present(freq_arr=freqs_arr):
+                res += len(s) - end  # remaining indices + current substr count
 
-            # While we have all required characters
-            while self._has_all_chars(freq):
-                # All substrings from current window to end are valid
-                # Add count of valid substrings
-                total += len(s) - right
+                freqs_arr[ord(s[start]) - ord("a")] -= 1  # we move start forward to slide window
+                start += 1
 
-                # Remove leftmost character and move left pointer
-                freq[ord(s[left]) - ord("a")] -= 1
-                left += 1
+            end += 1  # move end of window forward (IMP)
 
-            right += 1
-
-        return total
-
-    def _has_all_chars(self, freq: list) -> bool:
-        # Check if we have at least one of each character
-        return all(f > 0 for f in freq)
+        return res
 
 
 s = Solution()
+print(s.numberOfSubstrings("abcabc"))
 """
 The solution should follow more like a sliding window as once we find window with 'a', 'b' and 'c', we do constant operation to find the remaining substrings. So, we can
 optimise the solution to have single traversal.
