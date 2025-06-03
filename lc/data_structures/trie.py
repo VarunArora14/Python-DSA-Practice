bcf64279a070c5755a5d2d36b998a6ae5bb36c84
Varun Arora <varunarora1408@gmail.com>
2025-06-03 23:46:39 +0530

    trie implementation updated

--- lc/data_structures/trie.py
+++ lc/data_structures/trie.py
@@ -1,37 +1,56 @@
 class TrieNode:
-    def __init__(self):
+    def __init__(self, isLeaf=False):
         self.children = {}
-        self.is_end_of_word = False
+        self.isLeaf = isLeaf
 
 
 class Trie:
     def __init__(self):
         self.root = TrieNode()
-
+    
     def insert(self, word: str):
         node = self.root
-        for w in word:
-            if w not in node.children:
-                node.children[w] = TrieNode()
-            node = node.children[w]
-
-        node.is_end_of_word = True
-
-    def __search_helper(self, word: str):
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char] # go to char for next char to be added
+        node.isLeaf=True
+    
+    def search(self,word:str) -> bool:
+        if word == "":
+            return True
         node = self.root
-        for w in word:
-            if w not in node.children:
-                return None
-            node = node.children[w]
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        
+        return node.isLeaf
 
-        return node
-
-    def search(self, word: str):
-        res = self.__search_helper(word)
-        if res is None or not res.is_end_of_word:
-            return False
+    def contains_prefix(self, word:str) -> bool:
+        if word == "":
+            return True
+        
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        
         return True
+    
+t = Trie()
+t.insert("hello")
+t.insert("word")
+print(t.contains_prefix("hell"))
+print(t.contains_prefix("hello"))
+print(t.contains_prefix("hellow"))
+print(t.search("world"))
+print(t.search("word"))
+print(t.search("wordx"))
 
-    def starts_with(self, prefix: str):
-        res = self.__search_helper(word=prefix)
-        return res is not None
+
+            
+            
+                
+            
