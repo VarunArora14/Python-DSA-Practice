fe110f6d5136ef4e9f367a3ca27bf78a90d0b1b0
Varun Arora <varunarora1408@gmail.com>
2025-06-30 18:58:40 +0530

    refactor fourSum function to use a class method and improve duplicate handling

--- lc/18.py
+++ lc/18.py
@@ -1,22 +1,32 @@
-def foursum(nums: list[int], target: int) -> list[int]:
-  nums.sort()
-  res = []
-  for i in range(len(nums)):
-  for j in range(i+1, len(nums)):
-    for k in range(j+1, len(nums)):    
-    if nums[i] + nums[k] + nums[j] > target:
-      break
-    for l in range(k+1, len(nums)):
-      if nums[i] + nums[j] + nums[k] + nums[l] > target:
-      break # sorted array, no point going forward
-      if nums[i] + nums[j] + nums[k] + nums[l] == target:
-      arr= [nums[i], nums[j], nums[k], nums[l]]
-      if arr not in res:
-        res.append(arr)
-   
-  print(res)
-  return res 
-      
+from typing import List
 
 
-foursum([2,2,2,2,2],8)
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        res = []
+        vis = set()
+        n = len(nums)
+        for i in range(n - 3):
+            for j in range(i + 1, n - 2):
+                k, l = j + 1, n - 1
+                while k < l:
+                    if nums[i] + nums[j] + nums[k] + nums[l] == target:
+                        if (nums[i], nums[j], nums[k], nums[l]) not in vis:
+                            res.append((nums[i], nums[j], nums[k], nums[l]))
+                            vis.add((nums[i], nums[j], nums[k], nums[l]))
+                        k += 1
+                        l -= 1
+                    elif nums[i] + nums[j] + nums[k] + nums[l] > target:
+                        l -= 1
+                    else:
+                        k += 1
+        return res
+
+
+sol = Solution()
+nums = [1, 0, -1, 0, -2, 2]
+target = 0
+print(sol.fourSum(nums, target))
+
+# can add addition check of nums[i] == nums[i+1] for handling duplicates instead of set
